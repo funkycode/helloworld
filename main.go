dd44c00cffa076e2fb8f9b3ceb2d8b7a32c00e8b
Michael <michael.ketslah@tufin.com>
2020-06-29 11:29:42 +0300

    update according to lint suggestion

--- main.go
+++ main.go
@@ -12,23 +12,31 @@ func httpResponse(w http.ResponseWriter, r *http.Request) {
 	var request []byte
 	if r.Method != "POST" {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Only post supported"))
+		if _, err := w.Write([]byte("Only post supported")); err != nil {
+			log.Fatalf("Failed to return error msg. Error: %s", err)
+		}
 		return
 	}
 	request, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to read request. Error: " + err.Error()))
+		if _, err := w.Write([]byte("Failed to read request. Error: " + err.Error())); err != nil {
+			log.Fatalf("Failed to return error msg. Error: %s", err)
+		}
 		return
 	}
 	response, err := api.PingPong(request)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Failed to read request. Error: " + err.Error()))
+		if _, err := w.Write([]byte("Failed to read request. Error: " + err.Error())); err != nil {
+			log.Fatalf("Failed to return error msg. Error: %s", err)
+		}
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	w.Write(response)
+	if _, err := w.Write(response); err != nil {
+		log.Fatalf("Failed to return response. Error: %s", err)
+	}
 }
 
 func main() {
